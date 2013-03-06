ad3b9e07b75b87b917f5ffc8e0c6d94cf25e6060
Christine Borg <christineb102@gmail.com>
2013-03-06 12:28:49 +0100

    added objects and generated asteroids

--- unityAsteriods/Assets/AsteriodGenerator.js
+++ unityAsteriods/Assets/AsteriodGenerator.js
@@ -1,11 +1,38 @@
 #pragma strict
 
 var asteriodPrefab:Rigidbody;
+var numAsteroids:int;
+
+
+function generateAsteriods(maxasteroids:int){
+
+	var counter:int;
+	
+	for(counter=0;counter<maxasteroids;counter++)
+	{
+		var xcoord:float;
+		var ycoord:float;
+		
+		xcoord = Random.Range(BorderController.leftmost,BorderController.rightmost);
+		ycoord = Random.Range(BorderController.bottommost,BorderController.topmost);
+		Instantiate(asteriodPrefab,Vector3(xcoord,ycoord,3),Quaternion.identity);
+		//wait for one second per asteroid
+		yield WaitForSeconds(1);
+	}
+}
+
 
 function Start () {
-
+	generateAsteriods(numAsteroids);
 }
 
 function Update () {
+
+
+
+}
+
+function OnGUI () {
+
 
 }
